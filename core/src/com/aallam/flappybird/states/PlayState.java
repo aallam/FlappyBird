9b0c52f8e55248f26e15815337311dc067e3394e
Mouaad Aallam <Mouaad@Aallam.com>
2017-09-26 00:06:04 +0200

    [UPDATE] Play state background

--- core/src/com/aallam/flappybird/states/PlayState.java
+++ core/src/com/aallam/flappybird/states/PlayState.java
@@ -3,6 +3,7 @@ package com.aallam.flappybird.states;
 import com.aallam.flappybird.FlappyBird;
 import com.aallam.flappybird.sprites.Bird;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 /**
@@ -11,14 +12,17 @@ import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class PlayState extends State {
 
+  private static final String BACKGROUND = "background.png";
   private static final float WIDTH = FlappyBird.WIDTH / 2;
   private static final float HEIGHT = FlappyBird.HEIGHT / 2;
 
   private Bird bird;
+  private Texture background;
 
   public PlayState(GameStateManager gameStateManager) {
     super(gameStateManager);
     bird = new Bird(100, 300);
+    background = new Texture(BACKGROUND);
     camera.setToOrtho(false, WIDTH, HEIGHT);
   }
 
@@ -36,11 +40,13 @@ public class PlayState extends State {
   @Override public void render(SpriteBatch spriteBatch) {
     spriteBatch.setProjectionMatrix(camera.combined);
     spriteBatch.begin();
+    spriteBatch.draw(background, camera.position.x - camera.viewportWidth / 2, 0);
     spriteBatch.draw(bird.getTexture(), bird.getPosition().x, bird.getPosition().y);
     spriteBatch.end();
   }
 
   @Override public void dispose() {
     bird.dispose();
+    background.dispose();
   }
 }
