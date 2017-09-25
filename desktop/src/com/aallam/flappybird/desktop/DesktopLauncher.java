f456151840658efa7f456865c3daf420c5d013c8
Mouaad Aallam <Mouaad@Aallam.com>
2017-09-25 22:09:52 +0200

    init setup

---
+++ desktop/src/com/aallam/flappybird/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.aallam.flappybird.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.aallam.flappybird.FlappyBird;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FlappyBird(), config);
+	}
+}
