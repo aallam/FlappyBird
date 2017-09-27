f7890c28be8aa16daa07396631426be319605e44
Mouaad Aallam <Mouaad@Aallam.com>
2017-09-28 00:18:51 +0200

    [UPDATE] Landscape and menu state camera

--- core/src/com/aallam/flappybird/states/MenuState.java
+++ core/src/com/aallam/flappybird/states/MenuState.java
@@ -13,12 +13,15 @@ public class MenuState extends State {
 
   private static final String BACKGROUND = "background.png";
   private static final String PLAY_BUTTON = "button_play.png";
+  private static final float WIDTH = FlappyBird.WIDTH / 2;
+  private static final float HEIGHT = FlappyBird.HEIGHT / 2;
 
   private Texture background;
   private Texture playButton;
 
   public MenuState(GameStateManager gameStateManager) {
     super(gameStateManager);
+    camera.setToOrtho(false, WIDTH, HEIGHT);
     background = new Texture(BACKGROUND);
     playButton = new Texture(PLAY_BUTTON);
   }
@@ -34,12 +37,13 @@ public class MenuState extends State {
   }
 
   @Override public void render(SpriteBatch spriteBatch) {
+    spriteBatch.setProjectionMatrix(camera.combined);
     spriteBatch.begin();
     //Display the background
-    spriteBatch.draw(background, 0, 0, FlappyBird.WIDTH, FlappyBird.HEIGHT);
+    spriteBatch.draw(background, 0, 0);
     //Display the play button
-    float playButtonX = FlappyBird.WIDTH / 2 - playButton.getWidth() / 2;
-    float playButtonY = FlappyBird.HEIGHT / 2 - playButton.getHeight() / 2;
+    float playButtonX = camera.position.x - playButton.getWidth() / 2;
+    float playButtonY = camera.position.y - playButton.getHeight()/ 2;
     spriteBatch.draw(playButton, playButtonX, playButtonY);
     spriteBatch.end();
   }
